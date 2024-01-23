e7120c571d7a0f3c6edb8007523335797ed06345
Nihir2904 <nihirshah02@gmail.com>
2024-01-23 10:22:37 +0530

    bg image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div style={{backgroundImage: "url('https://unsplash.com/photos/a-black-and-white-photo-of-sand-and-water-JZgyf8bPdbY')", height: "100vh"}}>
+    <div style={{backgroundImage: "url('https://unsplash.com/photos/a-large-building-with-columns-and-a-clock-on-the-wall-nAo7sORl_q8')", height: "100vh"}}>
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch bg-cover bg-center">
     <h1 className="text-4x1 front-bold text-center">FloyDroid</h1>
       {messages.map(m => (
@@ -20,7 +20,7 @@ export default function Chat() {
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="mischief managed"
           onChange={handleInputChange}
         />
       </form>
